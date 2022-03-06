ccbfaed47378a73b8e3146f83f0307c043d435c0
Jeffrey Zhao <jeffrey@POTPAL-69.localdomain>
2022-03-06 11:09:29 -0600

    remove files, header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+
+function Header() {
+  return (
+    <div className="container pt-5">
+        <h1 className="text-center" style={{letterSpacing: '0.1em'}}>Image.Atomize</h1>
+        <hr/>
+    </div>
+  );
+}
+
+export default Header;
