8af3c6005cc463567261996e0be93bd3c7b40d13
Jeffrey Zhao <jeffrey@POTPAL-69.localdomain>
2022-03-06 17:41:35 -0600

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@
 function Header() {
   return (
     <div className="container pt-5">
-        <h1 className="text-center" style={{letterSpacing: '0.1em'}}>Image.Atomize</h1>
+        <h1 className="text-center font-monospace">Image.Atomize</h1>
         <hr/>
     </div>
   );
