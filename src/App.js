ab5b602d884a15fa161a92581e79b59794ad9790
Jeffrey Zhao <jeffreyzhao5618@gmail.com>
2022-03-14 14:38:53 -0500

    added What is this?

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AtomizeSection from './components/atomize_section/AtomizeSection';
 import Header from './components/Header'
+import mona from './mona_atomized.jpg'
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <AtomizeSection/>
+      <h1 className="text-center display-3">What is this?</h1>
+      <p className="lead mt-3">Image.Atomize lets you create images like the one below. Simply choose an image file, set its dimensions, and click the "Atomize!" button. Setting a higher dimension will create the modified image with more "cells" which makes the modified image look more like the original.</p>
+      <img className="mt-3 mx-auto d-block w-100" src={mona} style={{maxWidth:"800px"}}/>
     </div>
     
   );
