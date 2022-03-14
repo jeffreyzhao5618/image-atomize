146b55034dda6fb5da87620c2c40fdc8d646e2c4
Jeffrey Zhao <jeffreyzhao5618@gmail.com>
2022-03-14 15:04:11 -0500

    changed description

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Header />
       <AtomizeSection/>
       <h1 className="text-center display-3">What is this?</h1>
-      <p className="lead mt-3">Image.Atomize lets you create images like the one below. Simply choose an image file, set its dimensions, and click the "Atomize!" button. Setting a higher dimension will create the modified image with more "cells" which makes the modified image look more like the original.</p>
+      <p className="lead mt-3">Image.Atomize lets you create modified images like the one below that are composed of smaller versions of itself. Simply choose an image file, set its dimensions, and click the "Atomize!" button. Setting a higher dimension will create the modified image with more "cells" which makes the modified image look more like the original.</p>
       <img className="mt-3 mx-auto d-block w-100" src={mona} style={{maxWidth:"800px"}}/>
     </div>
     
